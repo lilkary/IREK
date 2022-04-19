b8b2d323f420076f7a53f6267aaad54394bfc480
jhonjgs <sanchezjhonjairo02@gmail.com>
2022-04-19 11:10:44 -0500

    https redirect

--- router/router.js
+++ router/router.js
@@ -7,6 +7,7 @@ router.all("/new-user", validator)
 
 
 router.get("/", (req, res)=> {
+  console.log(req.protocol)
   if(req.protocol == "http"){
     res.redirect("https://irek.herokuapp.com/")
     return
