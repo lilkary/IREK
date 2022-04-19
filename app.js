af5e8604f89f3ac3cd19ee02cbefcad408c888d1
jhonjgs <sanchezjhonjairo02@gmail.com>
2022-04-19 11:15:54 -0500

    https redirect

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express  = require("express")
 const mongoose = require("mongoose")
 const multer   = require("multer")
 const router = require("./router/router")
-const redirect = require("./middlewares/https.redirect")
 const PORT     = process.env.PORT || 3000
 const app      = express()
 
